b6a21d70acce15fc1a5547d44966a6e53206da01
sotskiydr <76069400+sotskiydr@users.noreply.github.com>
2021-07-02 22:29:43 +0000

    Deploying to gh-pages from @ sotskiydr/best-team-project@2be141c6a34d5c323320f90b0e1f22273373f2c1 🚀

--- src.a574bf54.js
+++ src.a574bf54.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/best-team-project/src.afcd1000.js.map
+//# sourceMappingURL=/best-team-project/src.a574bf54.js.map
